e1c717738d3457fe83f62d8a7314ee3bef802924
riazolol <kobehjy24@126.com>
2018-07-12 17:44:42 +0800

    add pid_increas

--- c_funtest/pid_LocationFunc.c
+++ c_funtest/pid_LocationFunc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//位置式PID计算
 typedef struct {
     float Kp, Ki, Kd; //定义pid系数变量
     float Ek, Ek1; //Ek--误差,Ek1--之前一次的误差
@@ -14,7 +15,7 @@ typedef struct {
  *      PIDLoc--pid位置(返回值);
  * ---------------------------------
  *  遗留问题:位置计算的累积误差不断增大,在debug时容易出现超调量,可以尝试Nichols方法整定pid参数
- *  ref:http://dictionary.sensagent.com/%E9%BD%90%E6%A0%BC%E5%8B%92%EF%BC%8D%E5%B0%BC%E7%A7%91%E5%B0%94%E6%96%AF%E6%96%B9%E6%B3%95/zh-zh/
+ * ref:http://dictionary.sensagent.com/%E9%BD%90%E6%A0%BC%E5%8B%92%EF%BC%8D%E5%B0%BC%E7%A7%91%E5%B0%94%E6%96%AF%E6%96%B9%E6%B3%95/zh-zh/
  * 
  */
 float PID_Loc(float SetValue,float ActualValue, PID_LockTypeDef *PID){
---
+++ c_funtest/pid_addonFunc.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+typedef struct {
+    //PID定义系数
+    float Kp, Ki, Kd;
+    /**
+     * Ek--当前误差ek
+     * Ek1--前一次误差e(k-1)
+     * Ek2--再前一次误差e(k-2)
+     * 
+    */
+    float Ek, Ek1,Ek2;
+    float Addsum;    
+}AddTypeDef;
+
+/**
+ * --------------------------------
+ * 增量式PID--只和当前及前两次误差;
+ * 参数:SetValue:设置值(预期值)
+ *     ActualValue:实际值(反馈值)
+ * --------------------------------
+ *     Addint:PID增量返回值
+ *     ps.最终返回值是增量值,在累积误差较大的情况下
+ * 
+*/
+float Add_cout(float SetValue, float ActualValue, AddTypeDef *PID) {
+    float Addint;  //反馈增量
+    PID->Ek = SetValue - ActualValue; //误差计算
+    Addint = PID->Kp * (PID->Ek1 - PID->Ek)+PID->Ki * PID->Ek + PID->Kd * (PID->Ek - 2 * PID->Ek1 + PID->Ek2);
+    /**
+     * 误差更新
+    */
+    PID->Ek1 = PID->Ek2;
+    PID->Ek1 = PID->Ek;
+    return Addint;
+}
