f44936526261fde70af105b263770f89178fedc8
xerezlolz <kobehjy24@126.com>
2019-05-24 06:45:47 +0800

    change commun.c 尾插

--- c_funtest/huaqing/commun.c
+++ c_funtest/huaqing/commun.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#define INIT 1
+#define INIT 2
 
 //创建节点
 typedef struct DulCom
@@ -37,11 +37,12 @@ DulList creat_comm(){
     return p;
 }
 
-//初始化,头插
+//初始化,尾插
 DulList init_comm(DulList l,int n){
     //char nm[10], sx[4];
-    int i, num;
-    DulList a;
+    int i;
+    DulList a, last;
+    last = l;
     for (i=0; i< INIT; i++){
         a = (DulList)malloc(sizeof(DulCommu));
         printf("\\==============================\\\n");
@@ -57,11 +58,18 @@ DulList init_comm(DulList l,int n){
         scanf("%s",a->sex);
         printf("\n");
 
+        last->next = a;
+        last = a;
+        /*
         a->next = l->next;
         l->next = a;
+        */
     }
+    last->next = NULL;
     return l;
 }
+
+//输出通讯录
 void print_comm(DulList p){
     int i = 1;
     DulList s;
